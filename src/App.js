244421ec638ab3d910c9dce167bb8141c3b9aeb1
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-09-30 17:23:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  /* function generateArr(num) {
+    const arr = []
+    let randomNum = Math.floor(Math.random() * 20)
+    for (let i = 0; i < num; i++) {
+      while (arr.some(item => item.back === randomNum)) {
+        randomNum = Math.floor(Math.random() * 20)
+      }
+      arr.push({back: randomNum, flipped: false})
+      arr.push({back: randomNum, flipped: false})
+      randomNum = Math.floor(Math.random() * 20)
+    }
+    return arr
+  }
+
+  function shuffleArr(arr) {
+    let currentIndex = arr.length, randomIndex
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+      [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]
+    }
+    return arr
+  } */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
