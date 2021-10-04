68b81e8cec8e5a45612781e97a0682c20b8acc0d
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-10-04 14:22:24 -0700

    WIP main menu UI - added button component

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,4 +1,5 @@
 import Logo from '../assets/logo.svg'
+import Button from './Button'
 
 const MainMenu = () => {
     return (
@@ -7,8 +8,28 @@ const MainMenu = () => {
             <div className="menu-wrapper">
                 <div className="selection">
                     <p>Select Theme</p>
-                    
+                    <div className="selection__buttons">
+                        <Button name="Numbers" />
+                        <Button name="Icons" />
+                    </div>
                 </div>
+                <div className="selection">
+                    <p>Number of Players</p>
+                    <div className="selection__buttons">
+                        <Button name="1" />
+                        <Button name="2" />
+                        <Button name="3" />
+                        <Button name="4" />
+                    </div>
+                </div>
+                <div className="selection">
+                    <p>Grid Size</p>
+                    <div className="selection__buttons">
+                        <Button name="4x4" />
+                        <Button name="6x6" />
+                    </div>
+                </div>
+                <Button name="Start Game" />
             </div>
         </div>
     )
